d81e7f448e6f795c96bee2be4d26d5dc81317f28
Mekediego <mekediego@gmail.com>
2025-11-05 01:21:02 -0600

    Definiendo código de operaciones
    Descripción de código d eoperaciones

--- Calculando área/Calculando área.cpp
+++ Calculando área/Calculando área.cpp
@@ -2,7 +2,27 @@
 // Created by meked on 05/11/2025.
 //
 #include <iostream> //Librerías de operaciones
+#include <locale>
 
 int main() {
-    
+    system("chcp 65001");              // Cambia la consola a UTF-8
+    std::locale::global(std::locale("")); // Activa configuración regional del sistema
+    int largo; //Declaramos una variable con int
+    int ancho; //Declaramos una variable con int
+    int area; //Declaramos una variable con int
+    int perimetro; //Declaramos una variable con int
+
+    std::cout << "Ingrese el largo: "; //Imprime en pantalla
+    std::cin >> largo; //Lee lo que se escriba en la consola
+    std::cout << "Ingrese el ancho: "; //Imprime en pantalla
+    std::cin >> ancho; //Lee lo que se escriba en la consola
+
+    perimetro = largo * 2 + ancho * 2;
+    area = largo * ancho;
+
+    //std:: cout <<"El perímetro es: " <<perimetro << std::endl; //std::endl; es para que baje el renglón.
+    //std:: cout <<"El área es: " <<area;
+
+    std:: cout <<"El perímetro es " <<perimetro << ", y el área es " <<area << ".";
+
 }
