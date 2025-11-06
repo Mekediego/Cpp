8fafa98915c0591e43d35d2d4885761f76416f48
Mekediego <mekediego@gmail.com>
2025-11-06 03:07:46 -0600

    Ejercicio de perímetro de triángulo
    Código listo para ejecutar el perímetro del triángulo

--- Calculando área/Calculando área.cpp
+++ Calculando área/Calculando área.cpp
@@ -7,11 +7,26 @@
 int main() {
     system("chcp 65001");              // Cambia la consola a UTF-8
     std::locale::global(std::locale("")); // Activa configuración regional del sistema
-    int largo, ancho, area, perimetro; //Declaramos una variable con int
+    int largo, ancho, area, perimetro; //Declaramos una variable con int Rectangulo
+    int LadoA, LadoB, LadoC, perimetroT; // Perímetro triángulo.
     //int ancho; //Declaramos una variable con int
     //int area; //Declaramos una variable con int
     //int perimetro; //Declaramos una variable con int
 
+std::cout <<"Generando programa para perímetro de triángulo" << std::endl;
+    std::cout << "Ingrese lado A: "; //Imprime en pantalla
+    std::cin >> LadoA; //Lee lo que se escrib
+    std::cout << "Ingrese lado B: "; //Imprime
+    std::cin >> LadoB; //Lee lo que se escribe
+    std::cout << "Ingrese lado C: "; //Imprime
+    std::cin >> LadoC; //Lee lo que se escribe
+
+    perimetroT = LadoA + LadoB + LadoC;
+
+
+    std::cout << "El perímetro del triángulo es " << perimetroT << " cm" << std::endl;
+    std::cout << std::endl;
+    std::cout <<"Perímetro y área de un cuadrado" << std::endl;
     std::cout << "Ingrese el largo: "; //Imprime en pantalla
     std::cin >> largo; //Lee lo que se escriba en la consola
     std::cout << "Ingrese el ancho: "; //Imprime en pantalla
